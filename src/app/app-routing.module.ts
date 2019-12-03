a3b8b545c1bd8605f436d4a2ece30b4635e60ef2
Victor Scarpin <vscarpin@clarika.com.ar>
2019-12-03 20:09:48 -0300

    Clase 23 leazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,9 @@ import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'posts', loadChildren: './pages/posts/posts.module#PostsModule'},
+  { path: '**', component: HomeComponent}
 
 ];
 
